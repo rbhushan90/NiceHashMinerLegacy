46f5e363a8a91ec16fc66f77600a3a85737eec3f
angelbbs <angelbbs@mail.ru>
2018-12-02 17:00:17 +0300

    11

--- NiceHashMiner/Program.cs
+++ NiceHashMiner/Program.cs
@@ -216,7 +216,21 @@ namespace NiceHashMiner
 
                     ConfigManager.GeneralConfig.ForkFixVersion = 10;
                 }
-                
+
+                if (Configs.ConfigManager.GeneralConfig.ForkFixVersion < 11)
+                {
+                    Helpers.ConsolePrint("NICEHASH", "Old version");
+                    if (Directory.Exists("internals"))
+                        Directory.Delete("internals", true);
+
+                    if (File.Exists("bin\\xmrig\\xmrig.exe"))
+                        File.Delete("bin\\xmrig\\xmrig.exe");
+
+                    if (File.Exists("bin_3rdparty\\t-rex\\t-rex.exe"))
+                        File.Delete("bin_3rdparty\\t-rex\\t-rex.exe");
+
+                    ConfigManager.GeneralConfig.ForkFixVersion = 11;
+                }
                 //**
 
                 // init active display currency after config load
