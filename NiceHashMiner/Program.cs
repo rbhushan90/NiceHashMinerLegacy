34ccb3e0f0f0650f5b08a0c754d586ee6872bd54
angelbbs <angelbbs@mail.ru>
2019-02-15 00:10:26 +0300

    13 released

--- NiceHashMiner/Program.cs
+++ NiceHashMiner/Program.cs
@@ -267,7 +267,13 @@ namespace NiceHashMiner
                         Directory.Delete("internals", true);
                     ConfigManager.GeneralConfig.ForkFixVersion = 12.1;
                 }
-
+                if (Configs.ConfigManager.GeneralConfig.ForkFixVersion < 13)
+                {
+                    Helpers.ConsolePrint("NICEHASH", "Old version");
+                    if (Directory.Exists("internals"))
+                        Directory.Delete("internals", true);
+                    ConfigManager.GeneralConfig.ForkFixVersion = 13;
+                }
                 //**
                 Thread.Sleep(500);
                 // init active display currency after config load
